d6dae38c57cb958d344d36b332fd92fe0b654305
Jason <jasonvachier@outlook.fr>
2024-10-21 20:55:14 +0200

    react

--- dashboard.php
+++ dashboard.php
@@ -24,17 +24,24 @@ $result = $conn->query($sql);
 
     <nav class="nav-header">
         <img class="icon" src="image/logo/logo.png">
-        <a class="header-link" href="index.php">Home</a>
-        <a class="header-link" href="about.php">About</a>
-        <a class="header-link" href="WRE.php">Why Renewable Energy</a>
-        <a class="header-link" href="articles.php">Blog</a>
-        <a class="header-link" href="leadership.html">Leadership</a>
+        <button class="menu-toggle" aria-label="Toggle menu">
+            <span class="menu-icon">☰</span> <!-- Icône de menu -->
+        </button>
+        
+        <div class="nav-links">
+            <a class="header-link" href="index.php">Home</a>
+            <a class="header-link" href="about.php">About</a> <!-- Mettez à jour le lien ici -->
+            <a class="header-link" href="WRE.php">Why Renewable Energy</a>
+            <a class="header-link" href="articles.php">Blog</a>
+            <a class="header-link" href="leadership.html">Leadership</a>
+        </div>
+
         <div style="margin-left: auto; text-align: right;">
             <?php if (isset($_SESSION['username'])): ?>
                 <span>Bienvenue, <?php echo htmlspecialchars($_SESSION['username']); ?></span>
-                <a href="logout.php" class="header-link" style="margin-left: 10px;">logout</a>
+                <a href="?logout=true" class="header-link" style="margin-left: 10px;">logout</a>
             <?php else: ?>
-                <a href="login.php" class="header-link" style="margin-left: 10px;">login</a>
+                <a href="login.html" class="header-link" style="margin-left: 10px;">login</a>
             <?php endif; ?>
         </div>
     </nav>
