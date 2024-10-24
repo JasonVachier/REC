a71c76f7e3085f68827d2665c9e4afe9cc547e36
PCjason <jasonvachier@outlook.fr>
2024-10-24 16:32:19 +0200

    create article 26

--- articles.php
+++ articles.php
@@ -63,7 +63,7 @@ $result = $conn->query($sql);
             <?php endif; ?>
 
             <?php if (isset($_SESSION['username'])): ?>
-                <a class="back-to-bashboard" href="dashboard.php">Back to dashboard</a>
+                <a href="dashboard.php">Back to dashboard</a>
             <?php endif; ?>
         </div>
     </main>
